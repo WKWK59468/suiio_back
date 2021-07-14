94f1062e85ceb0882ff38507c1b6d50051de2c67
GNYAR <s901121s901121@gmail.com>
2021-07-14 21:05:08 +0800

    Replace cadre to officer

--- models/officer.model.js
+++ models/officer.model.js
@@ -9,47 +9,47 @@ module.exports = {
     add: (req, callback) => {
         let data = req.body;
         let values = [data.sID, data.position];
-        sql = mysql.format('INSERT INTO `cadre`(`sID`, `position`) VALUES(?, ?);', values);
+        sql = mysql.format('INSERT INTO `officer`(`sID`, `position`) VALUES(?, ?);', values);
         return conn.query(sql, callback);
     },
     // delete officer
     delete: (req, callback) => {
         let data = req.body;
         let values = [data.sID, data.position];
-        sql = mysql.format('DELETE FROM `cadre` WHERE `sID` = ? AND `position` = ?;', values);
+        sql = mysql.format('DELETE FROM `officer` WHERE `sID` = ? AND `position` = ?;', values);
         return conn.query(sql, callback);
     },
     // fetch all officer
     fetchAll: (req, callback) => {
-        sql = mysql.format("SELECT * FROM `cadre` ORDER BY `authority` DESC;");
+        sql = mysql.format("SELECT * FROM `officer` ORDER BY `authority` DESC;");
         return conn.query(sql, callback);
     },
     // fetch by position
     fetchByPosition: (req, callback) => {
         let data = req.params;
         let values = [data.position];
-        sql = mysql.format("SELECT * FROM `cadre` WHERE `position` = ?;", values);
+        sql = mysql.format("SELECT * FROM `officer` WHERE `position` = ?;", values);
         return conn.query(sql, callback);
     },
     // fetch by authority
     fetchByAuthority: (req, callback) => {
         let data = req.params;
         let values = [data.authority];
-        sql = mysql.format("SELECT * FROM `cadre` WHERE `authority` = ?;", values);
+        sql = mysql.format("SELECT * FROM `officer` WHERE `authority` = ?;", values);
         return conn.query(sql, callback);
     },
     // update officer by position
     updateOfficer: (req, callback) => {
         let data = req.body;
         let values = [data.sID, data.position];
-        sql = mysql.format('UPDATE `cadre` SET `sID` = ? WHERE `position` = ?;', values);
+        sql = mysql.format('UPDATE `officer` SET `sID` = ? WHERE `position` = ?;', values);
         return conn.query(sql, callback);
     },
     // update authority by position
     updateAuthority: (req, callback) => {
         let data = req.body;
         let values = [data.authority, data.position];
-        sql = mysql.format('UPDATE `cadre` SET `authority` = ? WHERE `position` = ?;', values);
+        sql = mysql.format('UPDATE `officer` SET `authority` = ? WHERE `position` = ?;', values);
         return conn.query(sql, callback);
     }
 }
