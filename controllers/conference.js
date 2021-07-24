04cdb0a86419789f785972ef2b70393db0c7511e
jrong <wkwk59468@gmail.com>
2021-07-24 14:18:34 +0800

    fix upload(undone-2)

--- controllers/conference.js
+++ controllers/conference.js
@@ -1,126 +1,135 @@
-const models = require('../models/conference');
+const models = require("../models/conference");
 
 const errMessage = (status, err) => {
-    if (status == 500) {
-        return { "result": err }
-    }
-    if (status == 404) {
-        return { "result": "Not Found" }
-    }
-    if (status == 401) {
-        return { "result": "err" }
-    }
-    if (status == 403) {
-        return { "result": "UPDATE ERROR" }
-    }
-}
+  if (status == 500) {
+    return { result: err };
+  }
+  if (status == 404) {
+    return { result: "Not Found" };
+  }
+  if (status == 401) {
+    return { result: "err" };
+  }
+  if (status == 403) {
+    return { result: "UPDATE ERROR" };
+  }
+};
 const successMessage = {
-    "result": "true"
-}
+  result: "true",
+};
 
 class Conference {
-    list = (req, res) => {
-        models.list(req, (err, results) => {
-            if (err) {
-                res.status(500).json(errMessage(500, err));
-                return console.error(err);
-            }
-            if (!results.length) {
-                res.status(404).json(errMessage(404, err));
-                return console.error(err);
-            }
-            res.status(200).json(results);
-        })
-    }
-    fetchCategory = (req, res) => {
-        models.fetchBycategory(req, (err, results) => {
-            if (err) {
-                res.status(500).json(errMessage(500, err));
-                return console.error(err);
-            }
-            if (!results.length) {
-                res.status(404).json(errMessage(404, err));
-                return console.error(err);
-            }
-            res.status(200).json(results);
-        })
-    }
-    fetchContent = (req, res) => {
-        models.fetchOne(req, (err, results) => {
-            if (err) {
-                res.status(500).json(errMessage(500, err));
-                return console.error(err);
-            }
-            if (!results.length) {
-                res.status(404).json(errMessage(404, err));
-                return console.error(err);
-            }
-            res.status(200).json(results);
-        })
-    }
-    upload = (req, res) => {
-        models.upload(req, (err, results) => {
-                if (err) {
-                    res.status(500).json(errMessage(500, err));
-                    return console.error(err);
-                }
-                if (!results.affectedRows) {
-                    res.status(404).json(errMessage(401, err));
-                    return console.error(err);
-                }
-                res.status(200).json(successMessage);
-            })
-            //attendees
-        models.addAttendees(req, (err, results) => {
-                if (err) {
-                    res.status(500).json(errMessage(500, err));
-                    return console.error(err);
-                }
-                if (!results.affectedRows) {
-                    res.status(404).json(errMessage(401, err));
-                    return console.error(err);
-                }
-                res.status(200).json(successMessage);
-            })
-            //absentees
-        models.addAbsentees(req, (err, results) => {
-            if (err) {
-                res.status(500).json(errMessage(500, err));
-                return console.error(err);
-            }
-            if (!results.affectedRows) {
-                res.status(404).json(errMessage(401, err));
-                return console.error(err);
-            }
-            res.status(200).json(successMessage);
-        })
-    }
-    updateStatus = (req, res) => {
-        models.updateStatus(req, (err, results) => {
-            if (err) {
-                res.status(500).json(errMessage(500, err));
-                return console.error(err);
-            }
-            if (!results.affectedRows) {
-                res.status(404).json(errMessage(403, err));
-                return console.error(err);
-            }
-            res.status(200).json(successMessage);
-        })
-    }
-    updateContent = (req, res) => {
-        models.updateContent(req, (err, results) => {
-            if (err) {
-                res.status(500).json(errMessage(500, err));
-                return console.error(err);
-            }
-            if (!results.affectedRows) {
-                res.status(404).json(errMessage(403, err));
-                return console.error(err);
-            }
-            res.status(200).json(successMessage);
-        })
-    }
+  list = (req, res) => {
+    models.list(req, (err, results) => {
+      if (err) {
+        res.status(500).json(errMessage(500, err));
+        return console.error(err);
+      }
+      if (!results.length) {
+        res.status(404).json(errMessage(404, err));
+        return console.error(err);
+      }
+      res.status(200).json(results);
+    });
+  };
+
+  fetchCategory = (req, res) => {
+    models.fetchBycategory(req, (err, results) => {
+      if (err) {
+        res.status(500).json(errMessage(500, err));
+        return console.error(err);
+      }
+      if (!results.length) {
+        res.status(404).json(errMessage(404, err));
+        return console.error(err);
+      }
+      res.status(200).json(results);
+    });
+  };
+
+  fetchContent = (req, res) => {
+    models.fetchOne(req, (err, results) => {
+      if (err) {
+        res.status(500).json(errMessage(500, err));
+        return console.error(err);
+      }
+      if (!results.length) {
+        res.status(404).json(errMessage(404, err));
+        return console.error(err);
+      }
+      res.status(200).json(results);
+    });
+  };
+
+  upload = (req, res) => {
+    // models.upload(req, (err, results) => {
+    //   if (err) {
+    //     res.status(500).json(errMessage(500, err));
+    //     return console.error(err);
+    //   }
+    //   if (!results.affectedRows) {
+    //     res.status(404).json(errMessage(401, err));
+    //     return console.error(err);
+    //   }
+    //   res.status(200).json(successMessage);
+    // });
+    
+    //attendees
+    let attendees = req.body.attendees;
+    console.log(JSON.parse(attendees)[2]);
+    // models.addAttendees(req, (err, results) => {
+    //     if (err) {
+    //       res.status(500).json(errMessage(500, err));
+    //       return console.error(err);
+    //     }
+    //     if (!results.affectedRows) {
+    //       res.status(404).json(errMessage(401, err));
+    //       return console.error(err);
+    //     }
+    //     res.status(200).json(successMessage);
+    //   });
+
+    //   //absentees
+    // models.addAbsentees(req, (err, results) => {
+    //     if (err) {
+    //       res.status(500).json(errMessage(500, err));
+    //       return console.error(err);
+    //     }
+    //     if (!results.affectedRows) {
+    //       res.status(404).json(errMessage(401, err));
+    //       return console.error(err);
+    //     }
+    //     res.status(200).json(successMessage);
+    //   });
+  };
+
+  updateStatus = (req, res) => {
+    models.updateStatus(req, (err, results) => {
+      if (err) {
+        res.status(500).json(errMessage(500, err));
+        return console.error(err);
+      }
+      if (!results.affectedRows) {
+        res.status(404).json(errMessage(403, err));
+        return console.error(err);
+      }
+      res.status(200).json(successMessage);
+    });
+  };
+
+  updateContent = (req, res) => {
+    models.updateContent(req, (err, results) => {
+      if (err) {
+        res.status(500).json(errMessage(500, err));
+        return console.error(err);
+      }
+      if (!results.affectedRows) {
+        res.status(404).json(errMessage(403, err));
+        return console.error(err);
+      }
+      res.status(200).json(successMessage);
+    });
+  };
 }
 
 module.exports = new Conference();
--- models/conference.js
+++ models/conference.js
@@ -1,63 +1,88 @@
-const mysql = require('mysql');
-const conf = require('../conf');
+const mysql = require("mysql");
+const conf = require("../conf");
 
 const conn = mysql.createConnection(conf.db);
-let sql = '';
+let sql = "";
 
-const jsonObj = { "category": "3", "name": "聖誕晚會第二次會議", "date": "2021-07-25", "attatched_file": "christmas", "content": "表演獎金增加", "host": "活動長", "status": "0" };
+const jsonObj = {
+  category: "3",
+  name: "聖誕晚會第二次會議",
+  date: "2021-07-25",
+  attatched_file: "christmas",
+  content: "表演獎金增加",
+  host: "活動長",
+  status: "0",
+};
 
 module.exports = {
-    list: (req, callback) => {
-        sql = mysql.format('SELECT * FROM conference');
-        return conn.query(sql, callback);
-    },
-    fetchBycategory: (req, callback) => {
-        sql = mysql.format('SELECT conference.* FROM conference,category WHERE category.ID = ? AND conference.category = category.ID', [req.params.id]);
-        return conn.query(sql, callback);
-    },
-    fetchOne: (req, callback) => {
-        sql = mysql.format('SELECT * FROM conference WHERE conference.ID = ?', [req.params.id]);
-        return conn.query(sql, callback);
-    },
-    upload: (req, callback) => {
-        let body = JSON.parse(req.body.params);
-        let category = body.category;
-        let name = body.name;
-        let date = body.date;
-        let attached_file;
-        let content = body.content;
-        let host = body.host;
-        let status = "0";
+  list: (req, callback) => {
+    sql = mysql.format("SELECT * FROM conference");
+    return conn.query(sql, callback);
+  },
+  fetchBycategory: (req, callback) => {
+    sql = mysql.format(
+      "SELECT conference.* FROM conference,category WHERE category.ID = ? AND conference.category = category.ID",
+      [req.params.id]
+    );
+    return conn.query(sql, callback);
+  },
+  fetchOne: (req, callback) => {
+    sql = mysql.format("SELECT * FROM conference WHERE conference.ID = ?", [
+      req.params.id,
+    ]);
+    return conn.query(sql, callback);
+  },
+  upload: (req, callback) => {
+    let body = JSON.parse(req.body.params);
+    let category = body.category;
+    let name = body.name;
+    let date = body.date;
+    let attached_file;
+    let content = body.content;
+    let host = body.host;
+    let status = "0";
 
-        if (body.attached_file == null || body.attached_file == "") {
-            attached_file = null;
-        } else {
-            attached_file = body.attached_file;
-        }
-
-        sql = mysql.format('INSERT INTO conference(category,name,date,attached_file,content,host,status) VALUES(?,?,?,?,?,?,?)', [category, name, date, attached_file, content, host, status]);
-        return conn.query(sql, callback);
-    },
-    addAbsentees: (req, callback) => {
-        sql = mysql.format('INSERT INTO absentees(conference,absentees) VALUES(?,?)', [req.body.conference, req.body.absentees]);
-        return conn.query(sql, callback);
-    },
-    addAttendees: (req, callback) => {
-        sql = mysql.format('INSERT INTO absentees(conference,attendees) VALUES(?,?)', [req.body.conference, req.body.attendees]);
-        return conn.query(sql, callback);
-    },
-    updateStatus: (req, callback) => {
-        let status = req.body.status;
-        let id = req.params.id;
-
-        sql = mysql.format('UPDATE conference SET status=? WHERE ID=?', [status, id]);
-        return conn.query(sql, callback);
-    },
-    updateContent: (req, callback) => {
-        let content = req.body.content;
-        let id = req.params.id;
-
-        sql = mysql.format('UPDATE conference SET content=? WHERE ID=?', [content, id]);
-        return conn.query(sql, callback);
+    if (body.attached_file == null || body.attached_file == "") {
+      attached_file = null;
+    } else {
+      attached_file = body.attached_file;
     }
-}
+
+    sql = mysql.format(
+      "INSERT INTO conference(category,name,date,attached_file,content,host,status) VALUES(?,?,?,?,?,?,?)",
+      [category, name, date, attached_file, content, host, status]
+    );
+    return conn.query(sql, callback);
+  },
+  addAbsentees: (req, callback) => {
+    sql = mysql.format(
+      "INSERT INTO absentees SET ? ",
+      [req.body]
+    );
+    return conn.query(sql, callback);
+  },
+  addAttendees: (req, callback) => {
+    sql = mysql.format(
+      "INSERT INTO attendees SET ? ",
+      [req.body]
+    );
+    return conn.query(sql, callback);
+  },
+  updateStatus: (req, callback) => {
+    let status = req.body.status;
+    let id = req.params.id;
+
+    sql = mysql.format("UPDATE conference SET status=? WHERE ID=?", [
+      status,
+      id,
+    ]);
+    return conn.query(sql, callback);
+  },
+  updateContent: (req, callback) => {
+    let content = req.body.content;
+    let id = req.params.id;
+
+    sql = mysql.format("UPDATE conference SET content=? WHERE ID=?", [content,id]);
+    return conn.query(sql, callback);
+  },
+};
