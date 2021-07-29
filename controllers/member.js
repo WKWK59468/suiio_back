d6bae3f1404b853ea8b5e8e7c12bb7987f57a1c9
jrong <wkwk59468@gmail.com>
2021-07-30 02:43:42 +0800

    Fixed sID format

--- controllers/member.js
+++ controllers/member.js
@@ -51,14 +51,95 @@ class UserController {
         }
         //新增
     addMember = (req, res) => {
-            models.add(req, (err, results) => {
-                if (err) {
-                    res.status(500).json({ "result": "false" });
-                    return console.error(err);
+            const sID = req.body.sID;
+            if (sID.length == 10) {
+                const sIDArray = sID.split("");
+                if (sIDArray[0] == "1" || sIDArray[0] == "2" || sIDArray[0] == "3" || sIDArray[0] == "4") {
+                    if (sIDArray[1] == "1" || sIDArray[1] == "2" || sIDArray[1] == "3" || sIDArray[1] == "4" || sIDArray[1] == "6" || sIDArray[1] == "7" || sIDArray[1] == "8") {
+                        switch (sIDArray[5]) {
+                            case "0":
+                                if (sIDArray[6] == "1" || sIDArray[6] == "2" || sIDArray[6] == "3" || sIDArray[6] == "4" || sIDArray[6] == "5" || sIDArray[6] == "6" || sIDArray[6] == "7" || sIDArray[6] == "8") {
+                                    models.add(req, (err, results) => {
+                                        if (err) {
+                                            res.status(500).json({ "result": "false" });
+                                            return console.error(err);
+                                        }
+
+                                        res.json({ "result": "true" });
+                                    })
+                                } else {
+                                    res.status(500).json({ "result": "format error" });
+                                }
+                                break;
+                            case "2":
+                                if (sIDArray[6] == "1" || sIDArray[6] == "2" || sIDArray[6] == "3" || sIDArray[6] == "4") {
+                                    models.add(req, (err, results) => {
+                                        if (err) {
+                                            res.status(500).json({ "result": "false" });
+                                            return console.error(err);
+                                        }
+
+                                        res.json({ "result": "true" });
+                                    })
+                                } else {
+                                    res.status(500).json({ "result": "format error" });
+                                }
+                                break;
+                            case "3":
+                                if (sIDArray[6] == "1" || sIDArray[6] == "2" || sIDArray[6] == "3" || sIDArray[6] == "4") {
+                                    models.add(req, (err, results) => {
+                                        if (err) {
+                                            res.status(500).json({ "result": "false" });
+                                            return console.error(err);
+                                        }
+
+                                        res.json({ "result": "true" });
+                                    })
+                                } else {
+                                    res.status(500).json({ "result": "format error" });
+                                }
+                                break;
+                            case "4":
+                                if (sIDArray[6] == "1" || sIDArray[6] == "2" || sIDArray[6] == "3") {
+                                    models.add(req, (err, results) => {
+                                        if (err) {
+                                            res.status(500).json({ "result": "false" });
+                                            return console.error(err);
+                                        }
+
+                                        res.json({ "result": "true" });
+                                    })
+                                } else {
+                                    res.status(500).json({ "result": "format error" });
+                                }
+                                break;
+                            case "5":
+                                if (sIDArray[6] == "1" || sIDArray[6] == "2" || sIDArray[6] == "3") {
+                                    models.add(req, (err, results) => {
+                                        if (err) {
+                                            res.status(500).json({ "result": "false" });
+                                            return console.error(err);
+                                        }
+
+                                        res.json({ "result": "true" });
+                                    })
+                                } else {
+                                    res.status(500).json({ "result": "format error" });
+                                }
+                                break;
+                            default:
+                                res.status(500).json({ "result": "format error" });
+                        }
+                    } else {
+                        res.status(500).json({ "result": "format error" });
+                    }
+                } else {
+                    res.status(500).json({ "result": "format error" });
                 }
 
-                res.json({ "result": "true" });
-            })
+            } else {
+                res.status(500).json({ "result": "format error" });
+            }
         }
         //刪除user
     delMember = (req, res) => {
