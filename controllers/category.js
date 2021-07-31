dae80d27cb1b83619caa41c38bdb2da3c0285e57
jrong <wkwk59468@gmail.com>
2021-07-31 16:59:50 +0800

    Fixed category

--- controllers/category.js
+++ controllers/category.js
@@ -5,22 +5,25 @@ class categoryController {
     addCategory = (req, res) => {
         models.add(req, (err, results) => {
             if (err) {
-                res.status(500).json({ "result": "false" });
+                if (err.sqlState == "23000") {
+                    res.status(500).json({ "result": "Duplicate primary key" });
+                    return console.error(err);
+                }
+                res.status(500).json({ "result": err });
                 return console.error(err);
             }
 
-            res.json({ "result": "true" });
+            res.json({ "result": true });
         })
     }
     listCategory = (req, res) => {
         models.list(req, (err, results) => {
             if (err) {
-                res.status(500).json({ "result": "false" })
+                res.status(500).json({ "result": err })
                 return console.error(err);
             }
             if (!results.length) {
-                res.status(404).json({ "result": "false" });
-                console.log(err);
+                res.status(404).json({ "result": "There is nothing to show." });
                 return;
             }
             res.json(results);
@@ -29,29 +32,36 @@ class categoryController {
     delCategory = (req, res) => {
         models.del(req, (err, results) => {
             if (err) {
-                res.status(500).json({ "result": "false" });
+                res.status(500).json({ "result": err });
                 return console.error(err);
             }
-
-            res.json({ "result": "true" });
+            if (!results.affectedRows) {
+                res.status(404).json({ "result": "Can't find category." });
+                return console.error(err);
+            }
+            res.json({ "result": true });
         })
     }
     patchStatus = (req, res) => {
-        models.setStatus(req, function(err, results, fields) {
-            if (err) {
-                res.status(500).json({ "result": "false" });
-                return console.error(err);
-            }
+        const status = req.body.status;
+        if (status == 0 || status == 1) {
+            models.setStatus(req, function(err, results, fields) {
+                if (err) {
+                    res.status(500).json({ "result": err });
+                    return console.error(err);
+                }
 
-            if (!results.affectedRows) {
-                res.status(404).json({ "result": "false" });
-                console.log(err);
-                return;
-            }
-            res.status(200).json({ "result": "true" });
-        });
+                if (!results.affectedRows) {
+                    res.status(404).json({ "result": "Can't find category." });
+                    console.log(err);
+                    return;
+                }
+                res.status(200).json({ "result": true });
+            });
+        } else {
+            res.status(500).json({ "result": "Please Enter 0 or 1." });
+        }
     }
-
 
 }
 
