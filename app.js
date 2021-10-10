ef553d6c7ca4e8ff2a3c726cbe4550b40c51afb1
jrong <wkwk59468@gmail.com>
2021-10-10 17:06:20 +0800

    新增mysql-events套件監聽SQL事件

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const session = require('express-session');
+const MySQLEvents = require('@rodrigogs/mysql-events');
+const mysql = require('mysql');
+const conf = require('./conf');
+const conn = mysql.createConnection(conf.db);
 
 const index = require('./routes/index');
-const { Socket } = require('socket.io');
 
 const app = express();
 
@@ -31,6 +34,26 @@ app.use(session({
 
 //Router
 app.use('/api', index);
+
+// MySQLEventWatcher
+const MySQLEventWatcher = new MySQLEvents(conn, {
+    startAtEnd: true,
+    excludedSchemas: {
+        mysql: true,
+    },
+});
+MySQLEventWatcher.start().then(() => console.log('I\'m running!'))
+    .catch(err => console.error('Something bad happened', err));
+MySQLEventWatcher.addTrigger({
+    name: 'SuiioEvents',
+    expression: 'suiio.events',
+    statement: MySQLEvents.STATEMENTS.ALL,
+    onEvent: async (event) => {
+        console.log(event.affectedRows[0].after);   //事件
+    },
+})
+MySQLEventWatcher.on(MySQLEvents.EVENTS.CONNECTION_ERROR, console.error);
+MySQLEventWatcher.on(MySQLEvents.EVENTS.ZONGJI_ERROR, console.error);
 
 const server = http.createServer(app);
 //server Port
