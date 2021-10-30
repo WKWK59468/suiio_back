12b39693d22b942539fee92ff6d735ea603db0c5
jrong <wkwk59468@gmail.com>
2021-10-30 17:48:11 +0800

    日期不修改格式

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -1,15 +1,5 @@
 const eventsModels = require('../models/events.model');
 
-const dateFormat = (res) => {
-    res.forEach((element, index) => {
-        const Year = element.timestamp.getFullYear();
-        const Month = ((element.timestamp.getMonth() + 1) < 10) ? "0" + (element.timestamp.getMonth() + 1) : (element.timestamp.getMonth() + 1);
-        const Date = (element.timestamp.getDate() < 10) ? "0" + element.timestamp.getDate() : element.timestamp.getDate();
-        element.timestamp = Year + "-" + Month + "-" + Date;
-    });
-    return res;
-}
-
 class eventsController {
     add = (req, res) => {
         const body = req.body;
@@ -40,7 +30,6 @@ class eventsController {
         const sID = params.sID;
 
         eventsModels.fetch_comment(sID).then(async (result) => {
-            result = await dateFormat(result);
             res.status(200).json(result);
             return new Promise((resolve, reject) => { });
         }).catch((err) => {
