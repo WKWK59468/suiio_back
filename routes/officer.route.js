fe5f466006529b68812a8a417f7617c34569286a
jrong <wkwk59468@gmail.com>
2021-11-25 01:47:46 +0800

    officer,member route 新增登入判斷,修改登入判斷及jwt的payload

--- routes/officer.route.js
+++ routes/officer.route.js
@@ -1,15 +1,16 @@
 const officers = require("../controllers/officer.controller.js");
 const router = require("express").Router();
+const UserController = require("../controllers/member.controller");
 
 // add
-router.route("/add").post(officers.add);
+router.route("/add").post(UserController.check, officers.add);
 // delete
-router.route("/delete").post(officers.delete);
+router.route("/delete").post(UserController.check, officers.delete);
 // fetch
-router.route("/fetch/all").get(officers.fetchAll);
-router.route("/fetch/position/:position").get(officers.fetchByPosition);
-router.route("/fetch/permission/:permission").get(officers.fetchByPermission);
+router.route("/fetch/all").get(UserController.check, officers.fetchAll);
+router.route("/fetch/position/:position").get(UserController.check, officers.fetchByPosition);
+router.route("/fetch/permission/:permission").get(UserController.check, officers.fetchByPermission);
 // update
-router.route("/update/officer").post(officers.updateOfficer);
-router.route("/update/permission").post(officers.updatePermission);
+router.route("/update/officer").post(UserController.check, officers.updateOfficer);
+router.route("/update/permission").post(UserController.check, officers.updatePermission);
 module.exports = router;
