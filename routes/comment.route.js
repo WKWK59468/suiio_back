0aedb041f1bc441ae1753d10f8e2d609b54b5362
jrong <wkwk59468@gmail.com>
2021-11-27 18:34:23 +0800

    登入判斷完成 & 留言功能修正完成

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -5,11 +5,11 @@ const UserController = require("../controllers/member.controller");
 
 //POST
 router.route("/add/:tables").post(UserController.check, commentController.add);
-router.route("/update").post(UserController.check, commentController.update);
-router.route("/delete").post(UserController.check, commentController.delete);
+router.route("/update").post(UserController.check, UserController.check_permission_self, commentController.update);
+router.route("/delete").post(UserController.check, UserController.check_permission_self, commentController.delete);
 
 //GET
-router.route("/fetch/Bymember/:sID").get(commentController.fetchByMember);
+router.route("/fetch/Bymember/:sID").get(UserController.check, UserController.check_permission_self, commentController.fetchByMember);
 router.route("/fetch/all").get(commentController.fetchAll);
 router.route("/fetch/:tables/:tableID").get(commentController.fetchByID);
 
