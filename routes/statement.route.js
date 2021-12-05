28502281213e91dadba22aace428903e345c3446
jrong <wkwk59468@gmail.com>
2021-12-05 16:21:49 +0800

    活動類別關閉後不能新增資料

--- routes/statement.route.js
+++ routes/statement.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Statement = require("../controllers/statement.controller");
+const Category = require("../controllers/category.controller");
 
 router.route("/add/byMonth").post(Statement.addByMonth);
 router.route("/add/byCategory").post(Statement.addByCategory);
-router.route("/add/").post(Statement.add);
+router.route("/add/").post(Category.check, Statement.add);
 
 router.route("/delete").post(Statement.delete);
 router.route("/update").post(Statement.update);
